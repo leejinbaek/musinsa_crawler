6700a76ce833cd8a9e1775043638d7ba5259ad82
leejinbaek <wlsqor22222@naver.com>
2024-07-02 00:37:45 +0900

    complete

--- src/crawler.py
+++ src/crawler.py
@@ -25,10 +25,86 @@ def page_down(num):
         section.send_keys(Keys.PAGE_DOWN)
         time.sleep(0.01)
         
+##페이지 크롤링 함수
+def page_crawling(driver,limit, max_item_InList):
+    #저장공간 생성
+    clothes_db = []
+    
+    #seendata 생성
+    seen_data = set()
+    
+    #scroll_limit 설정
+    scroll_limit = 0
+    
+    while scroll_limit < limit and len(clothes_db) < max_item_InList:
+        content = driver.page_source
+        soup = BeautifulSoup(content, "html.parser")
+        page_down(1)
+        
+        #페이지 크롤링
+        clothes = soup.find_all("div" , class_="category__sc-rb2kzk-10 cjDxkP")
+        for cs in clothes:
+            shop = None
+            shop = cs.find("a", class_="category__sc-rb2kzk-11 kPDCPR")
+            if shop is not None:
+                shop = cs.find("a", class_="category__sc-rb2kzk-11 kPDCPR").text
+                
+            name = None
+            name = cs.find("a", class_="category__sc-rb2kzk-12 gBkfRU")
+            if name is not None:
+                name = cs.find("a", class_="category__sc-rb2kzk-12 gBkfRU").text
+                
+            og_price = None
+            og_price = cs.find("del", class_="category__sc-79f6w4-6 iHtcSg")
+            if og_price is not None:
+                og_price = og_price.text
+                
+            dc = None
+            dc = cs.find("strong", class_="category__sc-79f6w4-9 jNpLBZ")
+            if dc is not None:
+                dc = dc.text
+                
+            price = None
+            price = cs.find("span", class_="category__sc-79f6w4-5 eTRmwC")
+            if price is not None:
+                price = cs.find("span", class_="category__sc-79f6w4-5 eTRmwC").text
+            
+            sold_out = None
+            sold_out = cs.find("a", class_="category__sc-rb2kzk-11 WUSGE")
+            if sold_out is not None:
+                sold_out = "품절"
+            else:
+                sold_out = "재고 있음"
+            
+            clothes_data = {
+                "shop" : shop,
+                "name" : name,
+                "og_price" : og_price,
+                "dc" : dc,
+                "price" : price,
+                "sold_out" : sold_out
+            }
+            clothes_data_tuple = tuple(clothes_data.items())
+            if clothes_data_tuple not in seen_data:
+                seen_data.add(clothes_data_tuple)
+                clothes_db.append(clothes_data)
+            
+            if len(clothes_db) >= max_item_InList:
+                break
+        scroll_limit += 1
+        
+    return clothes_db
 
 # 메인
 def crawler():
     try:
+        while True:
+            try:
+                limit = int(input("수집할 데이터의 수를 입력하세요: "))
+                break
+            except ValueError:
+                print("올바른 숫자를 입력하세요")
+                
         print("해당 크롤러는 무신사 상품의 인기 상품들을 순서대로 보여줍니다.")
         print("최근 3개월간의 판매량을 기준으로 데이터를 수집합니다.")
         category = {
@@ -61,42 +137,17 @@ def crawler():
         if search_era != driver.find_element(By.XPATH, "//div[contains(@class, 'category__sc-')]"):
             raise ValueError("search_era not found")
         
-        #페이지 스크롤
-        page_down(100)
+        #크롤링
+        clothes_db = page_crawling(driver,1000,limit)
         
-        #soup 생성
-        content = driver.page_source
-        soup = BeautifulSoup(content, "html.parser")
-    
-        #저장공간 생성
-        clothes_db = []
+        #저장
+        with open(f"{keyword}_musinsa_top{limit}", "w", newline='', encoding='utf-8-sig') as file:
+            writer = csv.writer(file)
+            writer.writerow(["판매처", "제품명", "할인 전 판매가", "할인율", "판매가", "재고 상태"])
+            for clothes in clothes_db:
+                writer.writerow(clothes.values())
         
-        #페이지 크롤링
-        clothes = soup.find_all("div" , class_="category__sc-rb2kzk-10 cjDxkP")
-        for cs in clothes:
-            shop = cs.find("a", class_="category__sc-rb2kzk-11 kPDCPR").text
-            name = cs.find("a", class_="category__sc-rb2kzk-12 gBkfRU").text
-            og_price = None
-            og_price = cs.find("del", class_="category__sc-79f6w4-6 iHtcSg")
-            if og_price is not None:
-                og_price = og_price.text
-            dc = None
-            dc = cs.find("strong", class_="category__sc-79f6w4-9 jNpLBZ")
-            if dc is not None:
-                dc = dc.text
-            
-            price = cs.find("span", class_="category__sc-79f6w4-5 eTRmwC").text
-            
-            clothes_data = {
-                "shop" : shop,
-                "name" : name,
-                "og_price" : og_price,
-                "dc" : dc,
-                "price" : price
-            }
-            clothes_db.append(clothes_data)
-    
-        print(clothes_db)
+        print("CSV파일이 성공적으로 저장되었습니다.")
     
     except Exception as e:
         print("Error:", e)
